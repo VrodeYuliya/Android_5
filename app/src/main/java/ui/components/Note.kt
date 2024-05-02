5f88cbb3c04c0765f03084eef69906ce97734bca
VrodeYuliya <romanova.yulia5444@gmail.com>
2024-05-02 19:33:24 +0300

    update code Column in Note.kt

--- app/src/main/java/ui/components/Note.kt
+++ app/src/main/java/ui/components/Note.kt
@@ -14,8 +14,11 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.topic2.android.notes.R
 import com.topic2.android.notes.theme.rwGreen
 
@@ -43,8 +46,26 @@ fun Note() {
             .align(Alignment.CenterVertically)
         )
         {
-            Text(text = "Заголовок" , maxLines = 1)
-            Text(text = "Содержание", maxLines = 1)
+            Text(
+                text = "Заголовок" ,
+                color = Color.Black,
+                maxLines = 1,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 16.sp,
+                    letterSpacing = 0.15.sp
+              )
+            )
+            Text(
+                text = "Содержание",
+color = Color.Black.copy(alpha = 0.75f),
+                maxLines = 1,
+                style = TextStyle(
+                   fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp,
+                    letterSpacing = 0.25.sp
+                   )
+                )
         }
         Checkbox(
             checked = false,
